18f62880a96e127de6a4ad5482dba16f50558b9e
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2023-06-17 11:35:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
 
-        //hora = 18 //to test the conditionals
+    var myVar = setInterval(myTimer, 1000);
 
-        msg.innerHTML = `Agora são ${hora} horas.`
+    function myTimer() {
+        var tempo = new Date(), displayDate;
+        if (navigator.userAgent.toLowerCase().indexOf('edge') > -1) {
+            displayDate = tempo.toLocaleTimeString('pt-BR');
+        } else {
+            displayDate = tempo.toLocaleTimeString('pt-BR', { timeZone: 'America/Recife'});
+        }
 
+        msg.innerHTML = displayDate;
+
+        // hora = 21 // testes
 
         if (hora >= 0 && hora < 5) { //como resolver o problema de 00h?
             opa.innerText = 'Boa madrugada!'
@@ -32,3 +41,4 @@ function carregar() {
             // 73 96 108 1
         }
     }
+}
